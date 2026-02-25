6330ebf9670e832c524f74336335c5acd94dbe4c
Andrei <andrei@ultralytics.com>
2026-02-25 10:20:17 +0000

    bug fix

--- demo_creation/font_renderer.py
+++ demo_creation/font_renderer.py
@@ -97,7 +97,7 @@ def _draw_text_label(frame, text, position, font_size, box_color=None, text_colo
     frame = draw_rounded_rectangle(
         frame,
         (x, y),
-        (x + label_w, y + label_h+ int(label_padding/2)),
+        (x + label_w, y + label_h),
         box_color,
         thickness=-1,
         radius=2
