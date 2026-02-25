a7d442346a7d77a9f5fa28996fe77d79da128d34
Andrei <andrei@ultralytics.com>
2026-02-25 12:12:28 +0000

    bug fix

--- demo_creation/process_video.py
+++ demo_creation/process_video.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-def input_video(path):
+def input_video(path, writer_file_name='output.mp4'):
     if not os.path.isfile(path):
         raise FileNotFoundError(f"Input video file not found: {path}")
     
@@ -11,4 +11,7 @@ def input_video(path):
     fps = cap.get(cv2.CAP_PROP_FPS)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    return cap, fps, width, height
+    
+    out_writer = cv2.VideoWriter(writer_file_name, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+    return cap, fps, width, height, out_writer
+
