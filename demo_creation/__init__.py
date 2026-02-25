261997a54a15c20bdc7bc5539df39203e31cd6b3
Andrei <andrei@ultralytics.com>
2026-02-25 12:21:47 +0000

    process video adition

--- demo_creation/__init__.py
+++ demo_creation/__init__.py
@@ -2,5 +2,4 @@ from .font_renderer import draw_box_annotations
 from .frame_utils import brand_video, brand_frame
 from .process_video import input_video
 
-__all__ = ['draw_box_annotations', 'brand_video', 'brand_frame', 
-           'input_video', 'process_yolo_resutls_boundingbox' ]
+__all__ = ['draw_box_annotations', 'brand_video', 'brand_frame', 'input_video', 'process_yolo_resutls_boundingbox' ]
